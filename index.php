0ddd277ee4f55c6a9a4b9e42721f3db78d4f3a6f
opacopac <armand@tschanz.com>
2016-05-11 01:32:52 +0200

    expired traffic icons & favicon

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 	<title>NAV-Flightplan</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
 	<meta charset="utf-8">
+	<link rel="icon" type="image/png" href="icon/favicon.png" />
 	<link rel="stylesheet" href = "css/bootstrap.min.css">
 	<link rel="stylesheet" href = "css/ol.css">
 	<link rel="stylesheet" href = "css/arial-narrow.css" type="text/css" />
